0a542d4da0f88c616ded188077d3d6e908a5a419
Avanna <estebansegarra@knights.ucf.edu>
2024-03-28 14:43:47 -0400

    Adding files before checking

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ROS Image Publisher
+# Stereo ROS Image Publisher
 
 This node publishes all the images contained in an specified folder, in the specified topic.
 The images will be published in BGR8 format.
@@ -6,7 +6,7 @@ The images will be published in BGR8 format.
 ## Setup
 
 1. Clone this repo `git clone https://github.com/amc-nu/RosImageFolderPublisher`
-1. Change to repo dir `cd RosImageFolderPublisher`
+1. Change to repo dir `cd StereoRosImageFolderPublisher`
 1. Execute `catkin_make`
 1. Source the workspace `source devel/setup.bash`
 
@@ -23,11 +23,14 @@ or
 
 |Param name    | Type   | Description                                             | Default Value |
 |------------- |--------|-------------------------------------------------------  |---------------|
-|`topic_name`  | String | Name of the topic to publish the image stream           | `image_raw`   |
+|`topic_name_left`  | String | Name of the topic to publish the left image stream           | `image_raw_left`   |
+|`topic_name_right`  | String | Name of the topic to publish the right image stream           | `image_raw_right`   |
 |`publish_rate`| Integer| Frame rate in Hz to publish the image.                  | `10`          |
 |`sort_files`  | Boolean| Defines if the files will be sorted before publishing   | `True`        |
-|`frame_id`    | String | Sets the frame_id contained in the Image message header | `camera`      |
-|`image_folder`| String | Path to the folder containing the images to be published|               | 
+|`frame_id_left`    | String | Sets the frame_id_left contained in the Image message header | `camera_left`      |
+|`frame_id_right`    | String | Sets the frame_id_left contained in the Image message header | `camera_right`      |
+|`image_folder_left`| String | Path to the folder containing the images to be published|               | 
+|`image_folder_right`| String | Path to the folder containing the images to be published|               | 
 |`sleep`       | Int    | Sleep few seconds to make sure the images process nodes are started|`0` | 
 
 ### Example
@@ -39,3 +42,9 @@ or modify these in `publisher.launch`
 
 * Invalid files will be skipped. The supported image formats will correspond to the ones supported by CvBridge and OpenCV.
 * Publish Rate will depend on the speed of the Hard disk. 
+
+## Acknowledgements 
+
+This repository is a fork from the original repository at: https://github.com/amc-nu/RosImageFolderPublisher
+
+
--- src/image_folder_publisher/scripts/image_folder_publisher.py
+++ src/image_folder_publisher/scripts/image_folder_publisher.py
@@ -2,7 +2,7 @@
 from __future__ import print_function
 
 import roslib
-roslib.load_manifest('image_folder_publisher')
+roslib.load_manifest('stereo_image_folder_publisher')
 
 import sys
 import os
@@ -17,68 +17,121 @@ from cv_bridge import CvBridge, CvBridgeError
 
 class image_folder_publisher:
     def __init__(self):
-        self.__app_name = "image_folder_publisher"
-
+        self.__app_name = "stereo_image_folder_publisher"
         self._cv_bridge = CvBridge()
 
-        self._topic_name = rospy.get_param('~topic_name', '/image_raw')
-        rospy.loginfo("[%s] (topic_name) Publishing Images to topic  %s", self.__app_name, self._topic_name)
 
-        self._image_publisher = rospy.Publisher(self._topic_name, Image, queue_size=1)
+        # ROS Topics outputs for images
+        self._topic_name_left = rospy.get_param('~topic_name_left', '/image_raw_left')
+        rospy.loginfo("[%s] (topic_name) Publishing Images to topic  %s", self.__app_name, self._topic_name_left)
+        self._image_publisher_left = rospy.Publisher(self._topic_name_left, Image, queue_size=1)
 
+        self._topic_name_right = rospy.get_param('~topic_name_right', '/image_raw_right')
+        rospy.loginfo("[%s] (topic_name) Publishing Images to topic  %s", self.__app_name, self._topic_name_right)
+        self._image_publisher_right = rospy.Publisher(self._topic_name_right, Image, queue_size=1)
+
+        #ROS params
         self._rate = rospy.get_param('~publish_rate', 30)
         rospy.loginfo("[%s] (publish_rate) Publish rate set to %s hz", self.__app_name, self._rate)
 
         self._sort_files = rospy.get_param('~sort_files', True)
         rospy.loginfo("[%s] (sort_files) Sort Files: %r", self.__app_name, self._sort_files)
 
-        self._frame_id = rospy.get_param('~frame_id', 'camera')
-        rospy.loginfo("[%s] (frame_id) Frame ID set to  %s", self.__app_name, self._frame_id)
+        # ROS Camera topics
+        self._frame_id_left = rospy.get_param('~frame_id_left', 'camera_left')
+        rospy.loginfo("[%s] (frame_id) Frame ID set to  %s", self.__app_name, self._frame_id_left)
+
+        self._frame_id_right = rospy.get_param('~frame_id_right', 'camera_right')
+        rospy.loginfo("[%s] (frame_id) Frame ID set to  %s", self.__app_name, self._frame_id_right)
 
         self._loop = rospy.get_param('~loop', 1)
         rospy.loginfo("[%s] (loop) Loop  %d time(s) (set it -1 for infinite)", self.__app_name, self._loop)
 
-        self._image_folder = rospy.get_param('~image_folder', '')
+        # ROSrun folder parameters
+        self._image_folder_left = rospy.get_param('~image_folder_left', '')
+        self._image_folder_right = rospy.get_param('~image_folder_right', '')
 
         self._sleep = rospy.get_param('~sleep', 0.0)
         rospy.sleep(self._sleep)
         rospy.loginfo("[%s] (sleep) Sleep %f seconds after each image", self.__app_name, self._sleep)
         
-        if self._image_folder == '' or not os.path.exists(self._image_folder) or not os.path.isdir(self._image_folder):
+        #Section for loading the photos... Could use a dynamically loaded approach for times when pictures are incidentially loaded.         
+        if self._image_folder_left == '' or not os.path.exists(self._image_folder_left) or not os.path.isdir(self._image_folder_left):
             rospy.logfatal("[%s] (image_folder) Invalid Image folder", self.__app_name)
             sys.exit(0)
-        rospy.loginfo("[%s] Reading images from %s", self.__app_name, self._image_folder)
+        rospy.loginfo("[%s] Reading images from %s", self.__app_name, self._image_folder_left)
+
+        if self._image_folder_right == '' or not os.path.exists(self._image_folder_right) or not os.path.isdir(self._image_folder_right):
+            rospy.logfatal("[%s] (image_folder) Invalid Image folder", self.__app_name)
+            sys.exit(0)
+
+        rospy.loginfo("[%s] Reading images from %s", self.__app_name, self._image_folder_right)
+        
 
     def run(self):
         ros_rate = rospy.Rate(self._rate)
 
-        files_in_dir = [f for f in listdir(self._image_folder) if isfile(join(self._image_folder, f))]
+
+        files_in_dir_left = [f for f in listdir(self._image_folder_left) if isfile(join(self._image_folder_left, f))]
+        files_in_dir_right = [f for f in listdir(self._image_folder_right) if isfile(join(self._image_folder_right, f))]
+
+
         if self._sort_files:
-            files_in_dir.sort()
+            files_in_dir_left.sort()
+
+        if self._sort_files:
+            files_in_dir_right.sort()
+
+
+        if (len(files_in_dir_left) != len(files_in_dir_right)):
+            rospy.loginfo("[%s] Mismatched stereo image count: Left camera count:%d  right camera count: %d", self.__app_name, len(files_in_dir_left),len(files_in_dir_right))
+            return
+
         try:
             while self._loop != 0:
-                for f in files_in_dir:
+
+
+                # for f in files_in_dir:
+                for i in range(0,len(files_in_dir_left)):
                     if not rospy.is_shutdown():
-                        if isfile(join(self._image_folder, f)):
-                            cv_image = cv2.imread(join(self._image_folder, f))
-                            if cv_image is not None:
-                                ros_msg = self._cv_bridge.cv2_to_imgmsg(cv_image, "bgr8")
-                                ros_msg.header.frame_id = self._frame_id
-                                ros_msg.header.stamp = rospy.Time.now()
-                                self._image_publisher.publish(ros_msg)
-                                rospy.loginfo("[%s] Published %s", self.__app_name, join(self._image_folder, f))
+                        left_image_stereo_filename   = join(self._image_folder_left, files_in_dir_left[i])
+                        right_image_stereo_filename  = join(self._image_folder_right, files_in_dir_right[i])
+
+                        # if isfile(join(self._image_folder, f)):
+                        if(isfile(left_image_stereo_filename) and isfile(right_image_stereo_filename)):
+                            cv_image_left = cv2.imread(left_image_stereo_filename)
+                            cv_image_right= cv2.imread(right_image_stereo_filename)
+                            
+                            if (cv_image_left is not None and cv_image_right is not None):
+                                ros_msg_left = self._cv_bridge.cv2_to_imgmsg(cv_image_left, "bgr8")
+                                ros_msg_left.header.frame_id = self._frame_id
+                                ros_msg_left.header.stamp = rospy.Time.now()
+                                self._image_publisher_left.publish(ros_msg_left)
+                                rospy.loginfo("[%s] Published %s", self.__app_name, left_image_stereo_filename)
+
+                                ros_msg_right = self._cv_bridge.cv2_to_imgmsg(cv_image_right, "bgr8")
+                                ros_msg_right.header.frame_id = self._frame_id
+                                ros_msg_right.header.stamp = rospy.Time.now()
+                                self._image_publisher_left.publish(ros_msg_left)
+                                rospy.loginfo("[%s] Published %s", self.__app_name, right_image_stereo_filename)
                             else:
-                                rospy.loginfo("[%s] Invalid image file %s", self.__app_name, join(self._image_folder, f))
+                                rospy.loginfo("[%s] Invalid image file %s", self.__app_name, left_image_stereo_filename)
                             ros_rate.sleep()
                     else:
                         return
+
+
+
                 self._loop = self._loop - 1
+
+
+
         except CvBridgeError as e:
             rospy.logerr(e)
 
 
 def main(args):
-    rospy.init_node('image_folder_publisher', anonymous=True)
+    rospy.init_node('stereo_image_folder_publisher', anonymous=True)
 
     image_publisher = image_folder_publisher()
     image_publisher.run()
